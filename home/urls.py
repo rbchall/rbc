c5c951c5775b4c5d216c811c96f0fe709ba9b40a
sarbesh <sarbeshsarkarsks@gmail.com>
2018-10-18 07:37:20 +0530

    deleting branch Account

--- home/urls.py
+++ home/urls.py
@@ -1,8 +1,14 @@
 from django.urls import path, reverse
 from . import views
+from Account.views import login_view, signup_view, logout_view, dual
 
 app_name = 'home'
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('login', login_view, name='login'),
+    path('signup', signup_view, name='signup'),
+    path('logout', logout_view, name='logout'),
+    path('dual', dual, name='logout'),
+
 ]
