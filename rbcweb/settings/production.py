b64b85b19ba8e32bc0bd9e9f4eaf534a6add5039
sarbesh <sarbeshsarkarsks@gmail.com>
2018-11-08 10:24:03 +0530

    update prod for db

--- rbcweb/settings/production.py
+++ rbcweb/settings/production.py
@@ -92,6 +92,10 @@ DATABASES = {
     }
 }
 
+import dj_database_url
+
+db_from_env = dj_database_url.config()
+DATABASES['default'].update(db_from_env)
 
 # Password validation
 # https://docs.djangoproject.com/en/2.1/ref/settings/#auth-password-validators
